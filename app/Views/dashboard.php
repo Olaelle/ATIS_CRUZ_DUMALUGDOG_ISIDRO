e463086b87b95f708376770c21d53fd51ad36e36
Abstract Programmer <AbstractProgrammer@example.com>
2023-05-27 22:41:56 +0800

    first commit

---
+++ app/Views/dashboard.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Barangay MIS Dashboard</title>
+    <link rel="stylesheet" href="<?php echo base_url('assets/adminlte/css/adminlte.min.css'); ?>">
+</head>
+<body>
+    <div class="wrapper">
+        <!-- Navbar -->
+        <!-- Add your navbar code here -->
+
+        <!-- Sidebar -->
+        <!-- Add your sidebar code here -->
+
+        <!-- Content Wrapper -->
+        <div class="content-wrapper">
+            <!-- Add your dashboard content here -->
+            <h1>Welcome to the Barangay MIS Dashboard</h1>
+        </div>
+    </div>
+
---
+++ app/Views/resident.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Resident Information</title>
+    <link rel="stylesheet" href="<?php echo base_url('assets/adminlte/css/adminlte.min.css'); ?>">
+</head>
+<body>
+    <div class="wrapper">
+        <!-- Navbar -->
+        <!-- Add your navbar code here -->
+
+        <!-- Sidebar -->
+        <!-- Add your sidebar code here -->
+
+        <!-- Content Wrapper -->
+        <div class="content-wrapper">
+            <!-- Add your resident information content here -->
+            <h1>Resident Information</h1>
+        </div>
+    </div>
+
+    <script src="<?php echo base_url('assets/adminlte/js/adminlte.min.js'); ?>"></script>
+</body>
+</html>
